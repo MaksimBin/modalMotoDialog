c061e43cfd46c141c867197dacaa35ca7339dbbf
MaksimBin <130301328+MaksimBin@users.noreply.github.com>
2024-03-09 10:02:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,86 +1,113 @@
-
 var images = [];
+
 function preload() {
-    for (var i = 0; i < arguments.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.arguments[i];
-    }
+  for (var i = 0; i < arguments.length; i++) {
+    images[i] = new Image();
+    images[i].src = preload.arguments[i];
+  }
 }
 
-//-- usage --//
 preload(
-      "pngwing.com (13).png",
-      "pngwing.com (14).png",
-      "pngwing.com (15).png",
-      "pngwing.com (12).png"
+  "/pngwing.com (13).png",
+  "/pngwing.com (14).png",
+  "/pngwing.com (15).png",
+  "/pngwing.com (12).png"
 )
 
 
 let audios = []
-let audio = new Audio("Alec_Koff_-_Second_Law_(musmore.org).mp3")
+let audio = new Audio("/Alec_Koff_-_Second_Law_(musmore.org).mp3")
 
 audio.oncanplaythrough = isAppLoaded;
 
+audio.volume = 0.0
+
+let numberV = 0
+
+
+document.querySelector('.volume').innerHTML = `<button class="buttonD" onclick="audioVolume()"></button>`
+
+
+const audioVolume = () => {
+
+  if(numberV == 0) {
+
+document.querySelector('.volume').innerHTML = `<button class="buttonL" onclick="audioVolume()"></button>`
+numberV++
+audio.volume = 0.3
+console.log(audio.volume)
+  }
+
+  else{
+   document.querySelector('.volume').innerHTML = `<button class="buttonD" onclick="audioVolume()"></button>`
+   
+numberV--
+audio.volume = 0.0
+console.log(audio.volume)
+  }
+
+}
+
 function isAppLoaded() {
-   audios.push(audio.src)
+  audios.push(audio.src)
 }
 
 let btn = document.querySelector('.startButton')
 btn.disabled = true
 btn.innerHTML = "Загрузка..."
-  const bt = () => {
-  
-  
-    if (images.length == 4 && audios.length == 1) {
-      btn.disabled = false
-      btn.innerHTML = "Start"
-    } else {
-      btn.disabled = true
-      btn.innerHTML = "Загрузка..."
-    }
-    
-    console.log(images.length)
+const bt = () => {
+
+
+  if (images.length == 4 && audios.length == 1) {
+    btn.disabled = false
+    btn.innerHTML = "Start"
+  } else {
+    btn.disabled = true
+    btn.innerHTML = "Загрузка..."
   }
-  
-  
-  setInterval(() => {
-    bt()
-  }, 3000)
-  
-  
-  
-  
-  let numbers = 0
-  
-  
-  
-  
-  
-  
-  const audioPlay = () => {
-    audio.play()
-  }
-  
-  
-  const startDisplay = () => {
-    
-    
-    
-    
-    document.querySelector('.preloader').style = "opacity: 0.0; transition-duration: 2s;"
-    
-    
-    setTimeout(() => {
-      document.querySelector('.preloader').style = "display: none;"
-    }, 2000)
-    
-    
-    audioPlay()
-  }
+
+
+}
+
+
+setInterval(() => {
+  bt()
+}, 3000)
+
+
+
+
+let numbers = 0
+
+
+
+
+
+
+const audioPlay = () => {
+  audio.play()
+}
+
+
+const startDisplay = () => {
+
+
+
+
+  document.querySelector('.preloader').style = "opacity: 0.0; transition-duration: 2s;"
+
+
+  setTimeout(() => {
+    document.querySelector('.preloader').style = "display: none;"
+  }, 2000)
+
+
+  audioPlay()
+}
 
 setInterval(() => {
   document.querySelector('.content').style = `background-image: url('${images[getImages(numbers)].src}');background-repeat: no-repeat;background-size: contain;background-position: center;`
-  
+
 }, 1000)
 
 
@@ -97,48 +124,38 @@ const getImages = () => {
 document.querySelector('.content-el').style = "display: none;"
 
 const openModal = () => {
-  
+
   document.querySelector('.btn').style = "margin-top:350px;transform: rotate(90deg);transition-duration: 2s;"
-  
-  
+
+
   setTimeout(() => {
     document.querySelector('.btn').style = "margin-right: 600px;opacity: 0.0;transition-duration: 2s;"
   }, 2000)
-  
-  
+
+
   setTimeout(() => {
     document.querySelector('.btn').style = "display: none;"
   }, 3000)
-  
+
   setTimeout(() => {
     document.querySelector('.content-el').style = "display: block; margin-left:900px;"
   }, 3000)
-  
+
   setTimeout(() => {
     document.querySelector('.content-el').style = "margin-left:110px;transition-duration: 2s;"
   }, 4000)
 }
 
 const closeModal = () => {
- 
+
   document.querySelector('.content-el').style = "margin-left:900px;transition-duration: 2s;"
-  
-setTimeout(() => {
+
+  setTimeout(() => {
     document.querySelector('.btn').style = "margin-top: 1000px;opacity: 0.0;transition-duration: 2s;"
   }, 2000)
-  
+
   setTimeout(() => {
     document.querySelector('.btn').style = "margin-top: 350px;opacity: 0.9;transition-duration: 2s;"
   }, 3000)
 
 }
-
-
-
-
-
-
-
-
-
-
